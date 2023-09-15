95936e50b2b1dc5b9fb01367bbe97d90c88d0b27
it20 <it20@kulanoil.kz>
2023-09-15 09:25:59 +0600

    fixes

--- src/components/AdminHeader.vue
+++ src/components/AdminHeader.vue
@@ -1,37 +1,64 @@
 <template>
   <div
-      class="hidden lg:flex nav w-[250px] min-w-[250px] max-w-[250px] bg-white h-full p-5 flex-col justify-between shadow-lg border-r">
+    class="hidden lg:flex nav w-[250px] min-w-[250px] max-w-[250px] bg-white h-full p-5 flex-col justify-between shadow-lg border-r"
+  >
     <div>
-      <img alt="" class="w-full mb-20" src="@/assets/img/logo.png">
+      <img alt="" class="w-full mb-20" src="@/assets/img/logo.png" />
       <div class="">
-        <router-link :to="{ name: 'AdminPage' }" class="flex items-center hover:text-mainColor cursor-pointer mb-5">
-          <font-awesome-icon :icon="['fas', 'users']" class="mr-3 text-xl w-5"/>
+        <router-link
+          :to="{ name: 'AdminPage' }"
+          class="flex items-center hover:text-mainColor cursor-pointer mb-5"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'users']"
+            class="mr-3 text-xl w-5"
+          />
           <p>Пользователи</p>
         </router-link>
-        <router-link :to="{ name: 'AdminProducts' }" class="flex items-center hover:text-mainColor cursor-pointer mb-5">
-          <font-awesome-icon :icon="['fas', 'boxes-stacked']" class="mr-3 text-xl w-5"/>
+        <router-link
+          :to="{ name: 'AdminProducts' }"
+          class="flex items-center hover:text-mainColor cursor-pointer mb-5"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'boxes-stacked']"
+            class="mr-3 text-xl w-5"
+          />
           <p>Товары</p>
         </router-link>
-        <router-link :to="{ name: 'AdminCategories' }"
-                     class="flex items-center hover:text-mainColor cursor-pointer mb-5">
-          <font-awesome-icon :icon="['fas', 'tag']" class="mr-3 text-xl w-5"/>
+        <router-link
+          :to="{ name: 'AdminCategories' }"
+          class="flex items-center hover:text-mainColor cursor-pointer mb-5"
+        >
+          <font-awesome-icon :icon="['fas', 'tag']" class="mr-3 text-xl w-5" />
           <p>Категории</p>
         </router-link>
-        <router-link :to="{ name: 'AdminOrders' }" class="flex items-center hover:text-mainColor cursor-pointer mb-5">
-          <font-awesome-icon :icon="['fas', 'cart-shopping']" class="mr-3 text-xl w-5"/>
+        <router-link
+          :to="{ name: 'AdminOrders' }"
+          class="flex items-center hover:text-mainColor cursor-pointer mb-5"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'cart-shopping']"
+            class="mr-3 text-xl w-5"
+          />
           <p>Заказы</p>
         </router-link>
-        <router-link :to="{ name: 'AdminNews' }" class="flex items-center hover:text-mainColor cursor-pointer mb-5">
-          <font-awesome-icon :icon="['fas', 'newspaper']" class="mr-3 text-xl w-5"/>
+        <router-link
+          :to="{ name: 'AdminNews' }"
+          class="flex items-center hover:text-mainColor cursor-pointer mb-5"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'newspaper']"
+            class="mr-3 text-xl w-5"
+          />
           <p>Новости</p>
         </router-link>
       </div>
     </div>
     <div class="flex items-center justify-start">
-      <ThemeSwitcher/>
+      <ThemeSwitcher />
     </div>
-    <SidebarComponent/>
   </div>
+  <SidebarComponent />
 </template>
 
 <script>
@@ -49,7 +76,7 @@ export default {
   components: {
     SidebarComponent,
     ThemeSwitcher,
-    LocaleSwitcher
+    LocaleSwitcher,
   },
-}
+};
 </script>
